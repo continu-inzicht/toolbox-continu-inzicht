e5fa00ff04890697daec5d2b57d84f45d19dd44f
David Haasnoot <Daafips@gmail.com>
2024-09-10 16:48:54 +0200

    improving wikie

--- src/continu_inzicht_toolbox/continu_inzicht_toolbox/continu_inzicht_toolbox.py
+++ src/continu_inzicht_toolbox/continu_inzicht_toolbox/continu_inzicht_toolbox.py
@@ -16,8 +16,8 @@ class PlaceHolder(BaseModel):
 
         Parameters
         ----------
-
-        N: Number of times to print
+        n : int
+            Number of times to print statement
 
 
         Returns
@@ -28,7 +28,25 @@ class PlaceHolder(BaseModel):
         for i in range(n):
             print(f"{i}:{self.place} {self.holder}")
 
+    def count_placeholders_length(self):
+        """
+        Counts the length of the placeholders of the class
+
+        Parameters
+        ----------
+        None
+
+
+        Returns
+        -------
+        int
+            Number of charaters placeholders are.
+        """
+
+        return int(len(self.place) + len(self.holder))
+
 
 if __name__ == "__main__":
     c = PlaceHolder(place="a", holder="b")
     c.print_placeholders(2)
+    c.count_placeholders_length()
