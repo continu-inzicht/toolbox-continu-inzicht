79bae6a6027907c7fe6dbd8f72f6e250c5dfc62c
David Haasnoot <Daafips@gmail.com>
2024-09-24 13:37:11 +0200

    adjust module names

--- src/continu_inzicht_toolbox/__init__.py
+++ src/continu_inzicht_toolbox/__init__.py
@@ -1,9 +1,16 @@
 __version__ = "0.0.1"
 
-from continu_inzicht_toolbox.proof_of_concept import voorbeeld_module
-from continu_inzicht_toolbox.waterstanden import waterstanden
 from continu_inzicht_toolbox.base.config import Config
 from continu_inzicht_toolbox.base.data_adapter import DataAdapter
+from continu_inzicht_toolbox.proof_of_concept import voorbeeld_module
+from continu_inzicht_toolbox.waterstanden import haal_waterstanden_op
+from continu_inzicht_toolbox.fragility_curves import calculate_fragility_curves
 
 
-__all__ = ["voorbeeld_module", "waterstanden", "Config", "DataAdapter"]
+__all__ = [
+    "voorbeeld_module",
+    "haal_waterstanden_op",
+    "calculate_fragility_curves",
+    "Config",
+    "DataAdapter",
+]
