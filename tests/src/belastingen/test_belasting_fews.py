aee1fd6187da3631b1115e4d17d46421303379f8
RolfWaterman <waterman@hkv.nl>
2024-10-18 14:53:34 +0200

    Oplossen conflictmet sprint_2

--- tests/src/belastingen/test_belasting_fews.py
+++ tests/src/belastingen/test_belasting_fews.py
@@ -4,9 +4,11 @@ import pytest
 
 from datetime import datetime, timedelta, timezone
 from pathlib import Path
-from toolbox_continu_inzicht import Config, DataAdapter
-from toolbox_continu_inzicht import BelastingFews
+from toolbox_continu_inzicht.base.config import Config
+from toolbox_continu_inzicht.base.data_adapter import DataAdapter
+from toolbox_continu_inzicht.belastingen import BelastingFews
 
+@pytest.mark.skip(reason="Eerst een Fews rest service definieren")
 @pytest.mark.asyncio
 async def test_run():
   test_data_sets_path = Path(__file__).parent / "data_sets"  
