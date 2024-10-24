9ad8bfe5a90a310291c7020ac7f040c4c1b8ed45
David Haasnoot <Daafips@gmail.com>
2024-10-24 17:11:01 +0200

    run ruff

--- src/toolbox_continu_inzicht/loads/loads_waterinfo/get_waterinfo_thresholds.py
+++ src/toolbox_continu_inzicht/loads/loads_waterinfo/get_waterinfo_thresholds.py
@@ -1,7 +1,10 @@
 import pandas as pd
 from toolbox_continu_inzicht.utils.fetch_functions import fetch_data
 
-async def get_waterinfo_thresholds(location_code: str, parameter_id: str = "waterhoogte")-> pd.DataFrame:
+
+async def get_waterinfo_thresholds(
+    location_code: str, parameter_id: str = "waterhoogte"
+) -> pd.DataFrame:
     """Haal voor Waterinfo de thresholds op voor de opgegegeven parameter
 
     Args:
@@ -9,37 +12,35 @@ async def get_waterinfo_thresholds(location_code: str, parameter_id: str = "wate
 
     Returns:
         Dataframe: Pandas dataframe met thressholds
-    """    
+    """
     url: str = "https://waterinfo.rws.nl/api/chart/get"
 
     params = {
         "mapType": parameter_id,
         "locationCodes": location_code,
-        "values": "-48,48"
+        "values": "-48,48",
     }
 
     # Ophalen json data van de Waterinfo api
-    status, json_data = await fetch_data(
-        url=url, params=params, mime_type="json"
-    )
+    status, json_data = await fetch_data(url=url, params=params, mime_type="json")
 
-    # dataframe 
+    # dataframe
     dataframe = pd.DataFrame()
 
     if status is None and json_data is not None:
-        
-        if "limits" in json_data:         
+        if "limits" in json_data:
             dataframe = pd.DataFrame(json_data["limits"])
         else:
             raise ConnectionError("No thresholds found")
-        
+
     else:
-        raise ConnectionError("Connection failed")            
+        raise ConnectionError("Connection failed")
 
     return dataframe
 
 
-if __name__ == "__main__":  
+if __name__ == "__main__":
     import asyncio
+
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(get_waterinfo_thresholds("Aadorp(AADP)","waterhoogte"))
+    loop.run_until_complete(get_waterinfo_thresholds("Aadorp(AADP)", "waterhoogte"))
