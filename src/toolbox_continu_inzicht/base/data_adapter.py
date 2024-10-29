85f41e4d1e22b22dc41156dfc897026b97310c47
David Haasnoot <Daafips@gmail.com>
2024-10-29 16:32:08 +0100

    run ruff

--- src/toolbox_continu_inzicht/base/data_adapter.py
+++ src/toolbox_continu_inzicht/base/data_adapter.py
@@ -41,22 +41,29 @@ class DataAdapter(PydanticBaseModel):
 
     @staticmethod
     def validate_dataframe(df: pd.DataFrame, schema: dict):
-        
         expected_columns = list(schema.keys())
 
         actual_columns = df.columns.tolist()
         actual_dtypes = df.dtypes.to_dict()
-        
-        columns_match = set(expected_columns).issubset(set(actual_columns))        
-        dtypes_match = all(schema[key] == actual_dtypes[key] for key in schema if key in actual_dtypes)
-        
+
+        columns_match = set(expected_columns).issubset(set(actual_columns))
+        dtypes_match = all(
+            schema[key] == actual_dtypes[key] for key in schema if key in actual_dtypes
+        )
+
         if columns_match and dtypes_match:
             return 0, "DataFrame is geldig."
         else:
             if not columns_match:
-                return 1, f"Kolommen komen niet overeen. \nVerwachte kolommen: {schema}.\nHuidige kolommen: {actual_dtypes}."
+                return (
+                    1,
+                    f"Kolommen komen niet overeen. \nVerwachte kolommen: {schema}.\nHuidige kolommen: {actual_dtypes}.",
+                )
             if not dtypes_match:
-                return 2, f"Datatypes komen niet overeen.\nVerwachte kolommen: {schema}.\nHuidige kolommen: {actual_dtypes}."
+                return (
+                    2,
+                    f"Datatypes komen niet overeen.\nVerwachte kolommen: {schema}.\nHuidige kolommen: {actual_dtypes}.",
+                )
 
     def input(self, input: str, schema: dict = None):
         """Gegeven het config, stuurt de juiste input waarde aan
