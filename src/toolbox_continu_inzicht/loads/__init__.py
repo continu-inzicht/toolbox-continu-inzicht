a450598535e65194eb021d59caee069442a70dd6
RolfWaterman <waterman@hkv.nl>
2024-11-01 10:33:28 +0100

    __init__.py aangepast

--- src/toolbox_continu_inzicht/loads/__init__.py
+++ src/toolbox_continu_inzicht/loads/__init__.py
@@ -21,7 +21,6 @@ from toolbox_continu_inzicht.loads.loads_matroos.loads_matroos import (
 from toolbox_continu_inzicht.loads.loads_matroos.get_matroos_locations import (
     get_matroos_locations,
 )
-
 from toolbox_continu_inzicht.loads.loads_waterinfo.loads_waterinfo import (
     LoadsWaterinfo,
 )
@@ -32,6 +31,7 @@ from toolbox_continu_inzicht.loads.loads_waterinfo.get_waterinfo_thresholds impo
     get_waterinfo_thresholds,
 )
 from toolbox_continu_inzicht.loads.loads_classify.loads_classify import LoadsClassify
+from toolbox_continu_inzicht.loads.loads_whatif.loads_whatif import LoadsWhatIf
 
 
 __all__ = [
@@ -39,6 +39,7 @@ __all__ = [
     "LoadsFews",
     "LoadsMatroos",
     "LoadsWaterinfo",
+    "LoadsWhatIf",
     "get_rws_webservices_locations",
     "get_fews_locations",
     "get_fews_thresholds",
