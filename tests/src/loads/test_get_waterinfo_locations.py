c9a1afd222d49668a315603e3e5b258b72309e4c
David Haasnoot <Daafips@gmail.com>
2024-11-01 10:14:55 +0100

    fixing actually failing fews test

--- tests/src/loads/test_get_waterinfo_locations.py
+++ tests/src/loads/test_get_waterinfo_locations.py
@@ -1,12 +1,8 @@
 import pytest
-import os
 from toolbox_continu_inzicht.loads import get_waterinfo_locations
 
 
-@pytest.mark.skipif(
-    os.getenv("GITHUB_ACTIONS") == "true",
-    reason="Test has connection issue in Github Actions.",
-)
+@pytest.mark.asyncio
 async def test_get_waterinfo_locations():
     df = await get_waterinfo_locations(parameter_id="waterhoogte")
     assert df is not None
