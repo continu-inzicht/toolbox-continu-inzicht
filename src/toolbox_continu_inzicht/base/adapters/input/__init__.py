f2248d9ca6531791cfccf6ea67f90be664597de5
David Haasnoot <Daafips@gmail.com>
2026-01-06 16:24:47 +0100

    add example for FragilityCurveOvertoppingPreCalculated

--- src/toolbox_continu_inzicht/base/adapters/input/__init__.py
+++ src/toolbox_continu_inzicht/base/adapters/input/__init__.py
@@ -2,6 +2,7 @@ import warnings
 from toolbox_continu_inzicht.base.adapters.input.csv import *  # noqa: F403
 from toolbox_continu_inzicht.base.adapters.input.python import *  # noqa: F403
 
+# t
 try:
     from toolbox_continu_inzicht.base.adapters.input.netcdf import *  # noqa: F403
 except ImportError as e:
@@ -16,5 +17,10 @@ except ImportError as e:
 
 try:
     from toolbox_continu_inzicht.base.adapters.input.excel import *  # noqa: F403
+except ImportError as e:
+    warnings.warn(f"{e}.\n Some features may not be available.")
+
+try:
+    from toolbox_continu_inzicht.base.adapters.input.pre_calculated import *  # noqa: F403
 except ImportError as e:
     warnings.warn(f"{e}.\n Some features may not be available.")
