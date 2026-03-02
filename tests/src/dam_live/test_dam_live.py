c5b5dc94ddbf8f2900198cdc46980307bd43bb87
David Haasnoot <Daafips@gmail.com>
2026-03-02 12:02:19 +0100

    voeg docs en test toe

---
+++ tests/src/dam_live/test_dam_live.py
@@ -0,0 +1,37 @@
+# initialiseer de (toolbox continu inzicht) modules
+from pathlib import Path
+
+from toolbox_continu_inzicht.base.config import Config
+from toolbox_continu_inzicht.base.data_adapter import DataAdapter
+from toolbox_continu_inzicht.dam_live.merge_stage import StageMerger
+
+# ## Inladen van .json bestanden{#sec-Inladenjson}
+#
+# Dit voorbeeld laat zien hoe de verschillende mappen binnen het .stix bestand kunnen worden ingelezen doormiddel van data adapters. Er worden
+
+
+def setup_data_adapter():
+    test_data_path = Path(__file__).parent / "data_sets"
+    config = Config(config_path=test_data_path / "dam_live_parse_config.yaml")
+    config.lees_config()
+
+    return DataAdapter(config)
+
+
+def test_dam_live_parse():
+    data_adapter = setup_data_adapter()
+    merge_df = StageMerger(data_adapter=data_adapter)
+    merge_df.run(
+        input=[
+            "scenario",
+            "geometries",
+            "soils",
+            "soillayers",
+            "waternets",
+            "calculationsettings",
+        ],
+        output=["merge_soil", "merge_waternet", "merge_calculations"],
+    )
+    merge_df.df_merged_soils
+    merge_df.df_merged_waternet
+    merge_df.df_merged_calculations
